dab42ab48d3da9f4dc2dad0e1a156a3fedd1f272
berketonoz <btonoz@sabanciuniv.edu>
2024-04-01 22:36:19 +0300

    first commit

---
+++ CRUD/products.py
@@ -0,0 +1,16 @@
+from flask import Flask, jsonify, request
+import json
+from flask_cors import CORS
+
+app = Flask("Product Server")
+CORS(app)
+
+
+products = [
+    {'id': 143, 'name': 'Notebook', 'price': 5.49},
+    {'id': 144, 'name': 'Black Marker', 'price': 1.99}
+]
+
+# HTTP Methods
+
+app.run(port=5000,debug=True)
